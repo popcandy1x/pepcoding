32766086f3a946857f56d338c65c02cc38f3a6b6
Venkatesh Jaiswal <81379049+popcandy1x@users.noreply.github.com>
2022-03-10 10:45:55 +0530

    Update allindices.java

--- AllIndices/allindices.java
+++ AllIndices/allindices.java
@@ -1,12 +1,43 @@
-public class allindices{
-	public static void main(String[] args){
-		Scanner xed=new Scanner(System.in);
-		int t=xed.nextInt();
-		int[] a=new int[t];
-		for(i=0;i<a.length;i++)
-		{
-			a[i]=xed.nextInt();
-		}
-	}
-	public static void gw
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+        int x = Integer.parseInt(br.readLine());
+        int[] iarr = allIndices(arr, x, 0, 0);
+
+        if(iarr.length == 0){
+            System.out.println();
+            return;
+        }
+
+        for(int i = 0; i < iarr.length; i++){
+            System.out.println(iarr[i]);
+        }
+    }
+
+    public static int[] allIndices(int[] arr, int x, int idx, int fsf) {
+        if(idx==arr.length){
+            return new int[fsf];
+        }
+        if(arr[idx]==x){
+            int[] iarr=allIndices(arr,x,idx+1,fsf+1);
+            iarr[fsf]=idx;
+            return iarr;
+        }
+        else
+        {
+            int[] iarr=allIndices(arr,x,idx+1,fsf);
+            return iarr;
+        }
+    }
+
 }
